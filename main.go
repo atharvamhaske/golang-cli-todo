a047315516f8a8455b858223cdd33f58dec24fdc
atharvamhaske <atharvamhaske76@gmail.com>
2025-07-20 00:39:42 +0530

    finaly working line fine shyt

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 func main() {
-todos := Todos{}
-
-storage := NewStorage[Todos]("todos.json")
-storage.Load(&todos)
-
-cmdFlags := NewCmdFlags()
-cmdFlags.Execute(&todos)
-
-storage.Save(todos)
+	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
+	storage.save(todos)
 }
