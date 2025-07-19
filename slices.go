7cddc5898b6d6b6f34b40af03a5f627c35524090
atharvamhaske <atharvamhaske76@gmail.com>
2025-07-19 15:02:03 +0530

    practiced slices and append method in go

---
+++ slices.go
@@ -0,0 +1,25 @@
+package main 
+
+import "fmt"
+
+func main() {
+	numbers := []int {10, 20, 30, 40, 50}
+	indexToDelete := 2 
+
+    // get everything before number 2 and after 2 and make slices and append them later
+    before := numbers[:indexToDelete]
+
+	after := numbers[indexToDelete+1:]
+
+	// to join both slices using append
+
+	numbers = append(before, after...)
+
+	fmt.Println(numbers)  //Output: [10 20 40 50 ]
+
+
+ }
+
+// alternate one liner method to do in go idiom
+
+number = append(numbers[indexToDelete], numbers[indexToDelete+1:]...)
